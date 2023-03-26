57cd5d572cd8fe67511e418d98f8bd6dbd2f860a
Bassem Farrag <63361520+BassemFarrag@users.noreply.github.com>
2023-03-26 10:08:26 +0200

    Ueye-firstUpload

---
+++ assets/js/sub-page.js
@@ -0,0 +1,86 @@
+//Goals
+
+
+  document.querySelector("#show-goals-and-vision").addEventListener("click", function(){
+      document.querySelector(".goals-pop-up").classList.add("active");
+  });
+  document.querySelector(".goals-pop-up .close-btn").addEventListener("click", function(){
+      document.querySelector(".goals-pop-up").classList.remove("active");
+  });
+
+
+    document.querySelector("#show-goals-and-vision").addEventListener("click", function(){
+      document.querySelector(".blur-body").classList.add("active");
+  });
+  document.querySelector(".goals-pop-up .close-btn").addEventListener("click", function(){
+      document.querySelector(".blur-body").classList.remove("active");
+  });
+
+
+//Career
+
+    document.querySelector("#show-career-and-vision").addEventListener("click", function(){
+      document.querySelector(".career-pop-up").classList.add("active");
+  });
+  document.querySelector(".career-pop-up .close-btn").addEventListener("click", function(){
+      document.querySelector(".career-pop-up").classList.remove("active");
+  });
+
+
+    document.querySelector("#show-career-and-vision").addEventListener("click", function(){
+      document.querySelector(".blur-body").classList.add("active");
+  });
+  document.querySelector(".career-pop-up .close-btn").addEventListener("click", function(){
+      document.querySelector(".blur-body").classList.remove("active");
+  });
+
+
+//meet-team
+
+  document.querySelector("#show-meet-team-and-vision").addEventListener("click", function(){
+    document.querySelector(".meet-team-pop-up").classList.add("active");
+});
+document.querySelector(".meet-team-pop-up .close-btn").addEventListener("click", function(){
+    document.querySelector(".meet-team-pop-up").classList.remove("active");
+});
+
+  document.querySelector("#show-meet-team-and-vision").addEventListener("click", function(){
+    document.querySelector(".blur-body").classList.add("active");
+});
+document.querySelector(".meet-team-pop-up .close-btn").addEventListener("click", function(){
+    document.querySelector(".blur-body").classList.remove("active");
+});
+
+
+//terms
+
+  document.querySelector("#show-terms-and-vision").addEventListener("click", function(){
+    document.querySelector(".terms-pop-up").classList.add("active");
+});
+document.querySelector(".terms-pop-up .close-btn").addEventListener("click", function(){
+    document.querySelector(".terms-pop-up").classList.remove("active");
+});
+
+  document.querySelector("#show-terms-and-vision").addEventListener("click", function(){
+    document.querySelector(".blur-body").classList.add("active");
+});
+document.querySelector(".terms-pop-up .close-btn").addEventListener("click", function(){
+    document.querySelector(".blur-body").classList.remove("active");
+});
+
+
+//Who we are page
+
+document.querySelector("#who-we-are").addEventListener("click", function(){
+  document.querySelector(".we-pop-up").classList.add("active");
+});
+document.querySelector(".we-pop-up .close-btn").addEventListener("click", function(){
+  document.querySelector(".we-pop-up").classList.remove("active");
+});
+
+document.querySelector("#who-we-are").addEventListener("click", function(){
+  document.querySelector(".blur-body").classList.add("active");
+});
+document.querySelector(".we-pop-up .close-btn").addEventListener("click", function(){
+  document.querySelector(".blur-body").classList.remove("active");
+});
