d247ffe39ad464a8e925185e68c3bab770c3f11b
Bassem Farrag <63361520+BassemFarrag@users.noreply.github.com>
2023-05-20 14:03:02 +0300

    Edit the slogan

--- assets/js/slogan.js
+++ assets/js/slogan.js
@@ -4,14 +4,30 @@ const textLoad = () => {
       text.textContent = "...";
     }, 0);
     setTimeout(() => {
-      text.textContent = "Win";
-    }, 2000);
+      text.textContent = "win";
+    }, 1000);
     setTimeout(() => {
       text.textContent = "success";
+    }, 2000);
+    setTimeout(() => {
+      text.textContent = "improve";
+    }, 3000);
+    /*setTimeout(() => {
+      text.textContent = "";
     }, 4000);
     setTimeout(() => {
-      text.textContent = "Improve";
+      text.textContent = "";
+    }, 5000);
+    setTimeout(() => {
+      text.textContent = "";
     }, 6000);
+    setTimeout(() => {
+      text.textContent = "";
+    }, 7000);
+    setTimeout(() => {
+      text.textContent = "";
+    }, 8000);
+    */
   }
   textLoad();
   setInterval(textLoad, 12000);
